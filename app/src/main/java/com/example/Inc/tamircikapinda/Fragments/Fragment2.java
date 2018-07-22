be095b6539bb0c0ee3eea5b9b7fc93c63e06387c
denizcanilgin <39748708+denizcanilgin@users.noreply.github.com>
2018-07-22 18:02:54 +0300

    comitted by denizcan

--- app/src/main/java/com/example/Inc/tamircikapinda/Fragments/Fragment2.java
+++ app/src/main/java/com/example/Inc/tamircikapinda/Fragments/Fragment2.java
@@ -1,12 +1,18 @@
 package com.example.Inc.tamircikapinda.Fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.Inc.tamircikapinda.R;
 
@@ -27,6 +33,106 @@ public class Fragment2 extends android.support.v4.app.Fragment {
         usersPhone = view.findViewById(R.id.usersPhone);
 
         globalClass = ((GlobalClass) getActivity().getApplicationContext());
+
+        final Spinner disrict_spinner1 = (Spinner) view.findViewById(R.id.spinner_town1);
+        Spinner disrict_spinner2 = (Spinner) view.findViewById(R.id.spinner_town2);
+        final String[] disricts1 = new String[]{"İlçe seçiniz","Şahinbey İlçesi"};
+        String[] disricts2 = new String[]{"Mahalle seçiniz","Yeditepe Mahallesi","Güneykent Mahallesi","Binevler Mahallesi"};
+
+
+        ArrayAdapter disrictAdapter1 = new ArrayAdapter(getActivity(),android.R.layout.simple_spinner_dropdown_item,disricts1){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv1 = (TextView) view;
+                if(position == 0){
+
+                    tv1.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv1.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+
+
+        ArrayAdapter disrictAdapter2 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_dropdown_item,disricts2){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv2 = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv2.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv2.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+
+        //Set Adapter For Both Array to Spinner
+        disrict_spinner1.setAdapter(disrictAdapter1);
+        disrict_spinner2.setAdapter(disrictAdapter2);
+
+        disrict_spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                if(position > 0)
+//                   // Toast.makeText(getActivity(),"Seçiminiz : " + position,0).show();
+//                else
+//                    Toast.makeText(getActivity(),"Lütfen seçim yapınız! ",0).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        disrict_spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+               // Toast.makeText(getActivity(),"asdasdsad",0).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         usersAdress.addTextChangedListener(new TextWatcher() {
             @Override
--- app/src/main/java/com/example/Inc/tamircikapinda/SplashScreen.java
+++ app/src/main/java/com/example/Inc/tamircikapinda/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.Inc.tamircikapinda;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,17 +19,32 @@ public class SplashScreen extends Activity{
         setContentView(R.layout.splash);
         getWindow().setBackgroundDrawableResource(R.drawable.backg);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        ImageView upper_cable = (ImageView) findViewById(R.id.upper_cable);
+        ImageView lower_cable = (ImageView) findViewById(R.id.lower_cable);
+
+        upper_cable.setTranslationY(-1000f);
+        upper_cable.animate().translationYBy(1000f).setDuration(2000);
+
+        lower_cable.setTranslationY(+1000f);
+        lower_cable.animate().translationYBy(-1000f).setDuration(2000);
+
+
+//        Animation upper_cable_anim = AnimationUtils.loadAnimation(this,R.anim.enter_from_up);
+//        Animation lower_cable_anim = AnimationUtils.loadAnimation(this,R.anim.enter_from_down);
+
+
         iv = (ImageView) findViewById(R.id.iv);
-        Animation myanim= AnimationUtils.loadAnimation(this,R.anim.mytransition);
-        iv.startAnimation(myanim);
+        Animation logoAnim= AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        iv.startAnimation(logoAnim);
+//
         final Intent i =new Intent(this,Login.class);
-         Thread timer =new Thread(){
+        Thread timer =new Thread(){
             public void run(){
                 try {
-                    sleep(2500);
+                    sleep(2000);
                     startActivity(i);
-                }
-                catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
 
                 }

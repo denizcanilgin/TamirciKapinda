80e400c78ee3cfb82d249a1320d2559fd67c0caa
yesilbag <yesilbag.dev@gmail.com>
2018-07-22 17:23:39 +0300

    comitted by yesilbagApp

--- app/src/main/java/com/example/Inc/tamircikapinda/Home.java
+++ app/src/main/java/com/example/Inc/tamircikapinda/Home.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,14 +21,19 @@ import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.Inc.tamircikapinda.Fragments.MainFragment;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
     View view;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +48,17 @@ public class Home extends AppCompatActivity {
 
         Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),"Hoşgeldin"+" "+login_email,Snackbar.LENGTH_LONG);
         snackbar.show();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        FragmentActivity a = new FragmentActivity();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(a,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mDrawerLayout = findViewById(R.id.nav_menu_drawer_layout);
@@ -140,6 +158,9 @@ public class Home extends AppCompatActivity {
                     startActivity(i);
                     finish();
 
+                    Login login = new Login();
+                    login.signOut(mGoogleApiClient);
+
                 }
             });
             builder.setNegativeButton("İPTAL", new DialogInterface.OnClickListener(){
@@ -172,4 +193,8 @@ public class Home extends AppCompatActivity {
     public void onBackPressed() { }
 
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
--- app/src/main/java/com/example/Inc/tamircikapinda/Login.java
+++ app/src/main/java/com/example/Inc/tamircikapinda/Login.java
@@ -5,27 +5,49 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.Inc.tamircikapinda.Fragments.GlobalClass;
 import com.example.Inc.tamircikapinda.Models.Sonuc;
 import com.example.Inc.tamircikapinda.RestApi.ManagerAll;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class Login extends Activity {
+public class Login extends Activity implements
+        View.OnClickListener,
+        GoogleApiClient.OnConnectionFailedListener{
 
     private Button login, register_button;
     private AutoCompleteTextView login_email, login_sifre;
     public GlobalClass globalClass;
+
+    private static final String TAG = Login.class.getSimpleName();
+    private static final int RC_SIGN_IN = 007;
+
+    private GoogleApiClient mGoogleApiClient;
+    private SignInButton btnSignIn;
+    private LinearLayout gmail_access,facebook_access;
+
 
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
@@ -41,6 +63,9 @@ public class Login extends Activity {
         tanimla();
         login_control();
         RegisterActivity();
+        gmail_access();
+        facebook_access();
+
 
         globalClass = ((GlobalClass) getApplicationContext());
         preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());//preferences objesi
@@ -55,6 +80,23 @@ public class Login extends Activity {
             finish();
         }
         Log.i("valueeee",login_email+login_sifre);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        FragmentActivity a = new FragmentActivity();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(a,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Customizing G+ button
+        btnSignIn.setSize(SignInButton.SIZE_STANDARD);
+        btnSignIn.setScopes(gso.getScopeArray());
+
+
 }
 
     public void tanimla() {
@@ -63,8 +105,68 @@ public class Login extends Activity {
         login_email = findViewById(R.id.login_Email);
         login_sifre = findViewById(R.id.login_passwordd);
         register_button = findViewById(R.id.register_btn);
+        btnSignIn = findViewById(R.id.btn_sign_in);
+        gmail_access = findViewById(R.id.gmail_access);
+        facebook_access = findViewById(R.id.facebook_access);
 
     }
+
+    private void gmail_access()
+    {
+        gmail_access.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Gmail ile hızlı giriş özelliği önümüzdeki güncellemede aktif olacaktır.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void facebook_access()
+    {
+        facebook_access.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Facebook ile hızlı giriş özelliği önümüzdeki güncellemede aktif olacaktır.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+        Intent ıntent = new Intent(getApplicationContext(), Home.class);
+        startActivity(ıntent);
+    }
+
+    public void signOut(GoogleApiClient mGoogleApiClientt) {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClientt).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        Toast.makeText(getApplicationContext(), ""+status, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            Log.e(TAG, "display name: " + acct.getDisplayName());
+
+            String personName = acct.getDisplayName();
+            String personPhotoUrl = "";//acct.getPhotoUrl().toString();
+            String email = acct.getEmail();
+
+            Toast.makeText(this, ""+personName +"\n"+email, Toast.LENGTH_SHORT).show();
+
+        } else {
+            // Signed out, show unauthenticated UI.
+        }
+    }
+
 
     public void RegisterActivity() {
 
@@ -73,6 +175,8 @@ public class Login extends Activity {
             public void onClick(View view) {
                 Intent intent = new Intent(Login.this,Register.class);
                 startActivity(intent);
+
+
                 GlobalClass.setLogin_email(null);
                 GlobalClass.setLogin_password(null);
             }
@@ -125,9 +229,7 @@ public class Login extends Activity {
         text = text.replace("Ğ", "G");
         text = text.replace("Ç", "C");
 
-
         return text;
-
     }
 
     public void deleteFromEdittext() {
@@ -173,6 +275,63 @@ public class Login extends Activity {
         });
 
 
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        int id = view.getId();
+
+        switch (id) {
+            case R.id.btn_sign_in:
+
+
+                Toast.makeText(this, "Bu özellik önümüzdeki versiyonlarda aktif olacaktır", Toast.LENGTH_SHORT).show();
+
+//                signIn();
+//
+//                OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+//                if (opr.isDone()) {
+//
+//                    Log.d(TAG, "Got cached sign-in");
+//                    GoogleSignInResult result = opr.get();
+//                    handleSignInResult(result);
+//                } else {
+//
+//                    opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+//                        @Override
+//                        public void onResult(GoogleSignInResult googleSignInResult) {
+//                            //hideProgressDialog();
+//                            handleSignInResult(googleSignInResult);
+//                        }
+//                    });
+//                }
+                break;
+
+        }
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mGoogleApiClient.connect();
     }
 
 }
